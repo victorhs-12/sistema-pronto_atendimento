58672b3fe462332e22f64da320fc0e856d4792c9
Marlon-Lopes-Rodrigues <marlonlopes907@gmail.com>
2026-05-01 20:26:32 -0300

    modularização do sistema e implementação de arquivos de cabeçalho (.h)

--- Pilhas/pilha.c
+++ Pilhas/pilha.c
@@ -3,21 +3,21 @@
 #include "pilha.h"
 
 void inicializarPilha(Pilha *p) {
-    p->topo = NULL; // Inicializa a pilha vazia
+    p->topo = NULL;
 }
 
 void empilhar(Pilha *p, paciente *paciente) {
-    paciente->prox = p->topo; // O novo nó aponta para o antigo topo[cite: 18]
-    p->topo = paciente;       // O topo agora é o novo nó[cite: 18]
+    paciente->prox = p->topo;
+    p->topo = paciente; 
 }
 
 paciente* desempilhar(Pilha *p) {
-    if (p->topo == NULL) return NULL; // Proteção contra Underflow[cite: 18]
+    if (p->topo == NULL) return NULL;
     
     paciente *temp = p->topo;
-    p->topo = p->topo->prox; // Avança o topo para o próximo nó[cite: 18]
-    temp->prox = NULL;       // Isola o nó removido
-    return temp;             // Retorna o endereço para reinserção na fila[cite: 1]
+    p->topo = p->topo->prox; 
+    temp->prox = NULL;    
+    return temp;        
 }
 
 void mostrarPilha(Pilha *p) {
@@ -25,13 +25,13 @@ void mostrarPilha(Pilha *p) {
     printf("\n--- HISTÓRICO DE PACIENTES ATENDIDOS ---\n");
     while (aux != NULL) {
         printf("Nome: %-20s | Prioridade: %d\n", aux->nome_paciente, aux->prioridade);
-        aux = aux->prox; // Navegação sequencial[cite: 18]
+        aux = aux->prox;
     }
 }
 
 void destruirPilha(Pilha *p) {
     while (p->topo != NULL) {
         paciente *temp = desempilhar(p);
-        free(temp); // Liberação sistemática de cada nó[cite: 18, 39]
+        free(temp);
     }
 }
--- Pilhas/pilha.h
+++ Pilhas/pilha.h
@@ -1,17 +1,16 @@
 #ifndef PILHA_H
 #define PILHA_H
 
-#include "paciente.h" // Importante para usar a struct comum do projeto
+#include "paciente.h"
 
 typedef struct {
-    paciente *topo; // Ponteiro para o nó do topo
+    paciente *topo; 
 } Pilha;
 
-// Protótipos das operações[cite: 35]
 void inicializarPilha(Pilha *p);
 void empilhar(Pilha *p, paciente *paciente);
-paciente* desempilhar(Pilha *p); // Vital para a função "Desfazer"
+paciente* desempilhar(Pilha *p);
 void mostrarPilha(Pilha *p);
-void destruirPilha(Pilha *p); // Obrigatório para evitar vazamento de memória[cite: 18, 39]
+void destruirPilha(Pilha *p);
 
 #endif
